6165e4f4248a20f0ef1ef2f85b3717ae619b621a
Abraham Belayneh <epicabe145@gmail.com>
2021-05-02 18:38:35 -0400

    day 15

---
+++ Intermediate Projects/OOP.py
@@ -0,0 +1,34 @@
+# Imagine getting tasked for a self-driving car
+
+# In object-oriented, you would break up each component of the car into different parts. Camera, lane detection, and so on.
+
+# An object would just combine an attribute and a method
+
+# Split tasks to pieces
+
+# How to use OOP
+
+# Delegate tasks to other sections
+# Waiter has attributes and methods
+
+# An attribute
+is_holding_plate = True
+tables_responsible = [4, 7, 7]
+
+# A method
+
+
+def take_order(table, order):
+    print(is_holding_plate)
+
+
+def take_payment(amount):
+    print(tables_responsible[0])
+
+
+def carobectiverunhere():
+    print()
+# Object            Class
+
+
+car = carobectiverunhere()
